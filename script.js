60db366fa0e28e68b005067fbaf8c81c1489ac76
BlackFury117 <idrissadiallo97@icloud.com>
2024-02-05 12:46:16 +0000

    Finishing the navbar for the mobile view

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ window.onload = function(){
         });
     }
 };
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerCheckbox = document.querySelector(".hamburger input");
+    const navLinks = document.querySelector(".nav-links");
+  
+    hamburgerCheckbox.addEventListener("change", function () {
+      if (this.checked) {
+        navLinks.style.display = "block";
+        document.body.style.overflow = "hidden";
+        navLinks.classList.add("show");
+      } else {
+        navLinks.style.display = "none";
+        document.body.style.overflow = "auto";
+        navLinks.classList.remove("show");
+      }
+    });
+});
